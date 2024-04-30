9d7a0629c4e338d3c70e88bc17d1e7c01bc53bd3
Andrés Díaz <diazandres756@gmail.com>
2024-04-30 09:45:27 -0500

    subiendo el projecto

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Movie Matrix");
+
+        int releaseDate = 1999;
+        double evaluation = 4.5;
+        boolean incluidoEnElPlanBasico = true;
+        String sinopsis = """
+                La mejor
+                Pelicula
+                """;
+
+        System.out.println(releaseDate+evaluation);
+        System.out.println(incluidoEnElPlanBasico);
+        System.out.println(sinopsis);
+    }git
+}
